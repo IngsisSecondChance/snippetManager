ce44a6c0a178d2c2f2f306b64598344c3a454220
rcastromartinez <rcastromartinez@mail.austral.edu.ar>
2025-09-30 18:31:19 -0300

    chore: migrate to Kotlin DSL, add Spotless, Checkstyle, JaCoCo and CI pipeline

--- src/test/java/ingsis/snippet/SnippetApplicationTests.java
+++ src/test/java/ingsis/snippet/SnippetApplicationTests.java
@@ -2,12 +2,25 @@ package ingsis.snippet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class SnippetApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Container static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16");
 
+  @DynamicPropertySource
+  static void overrideProps(DynamicPropertyRegistry r) {
+    r.add("spring.datasource.url", postgres::getJdbcUrl);
+    r.add("spring.datasource.username", postgres::getUsername);
+    r.add("spring.datasource.password", postgres::getPassword);
+  }
+
+  @Test
+  void contextLoads() {}
 }
