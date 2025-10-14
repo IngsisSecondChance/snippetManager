df8e66faf0ee397727390c0591651b3af91d637b
rcastromartinez <rcastromartinez@mail.austral.edu.ar>
2025-10-14 18:44:35 -0300

    docker

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,9 +27,11 @@ dependencies {
 
 	runtimeOnly("org.postgresql:postgresql")
 
+	// Test dependencies
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.testcontainers:junit-jupiter:1.20.1")
-	testImplementation("org.testcontainers:postgresql:1.20.1")
+	testImplementation(platform("org.testcontainers:testcontainers-bom:1.20.1"))
+	testImplementation("org.testcontainers:junit-jupiter")
+	testImplementation("org.testcontainers:postgresql")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
