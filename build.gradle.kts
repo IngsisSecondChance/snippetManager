0413413f2744d9829ee73f93aaa39e885bad66af
rcastromartinez <rcastromartinez@mail.austral.edu.ar>
2025-10-14 18:48:24 -0300

    docker

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
 	// Test dependencies
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation(platform("org.testcontainers:testcontainers-bom:1.20.1"))
+	testImplementation("org.testcontainers:testcontainers")
 	testImplementation("org.testcontainers:junit-jupiter")
 	testImplementation("org.testcontainers:postgresql")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
